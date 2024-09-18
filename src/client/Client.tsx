23e5fb6009b446bf3d074dbb906729da1c1e83cb
Bekbolot <justworking8888@gmail.com>
2024-09-18 17:17:34 +0600

    sex

--- src/client/Client.tsx
+++ src/client/Client.tsx
@@ -22,7 +22,7 @@ const Client: React.FC = () => {
     const [loadingAreas, setLoadingAreas] = useState(false);
     const [loadingConsultants, setLoadingConsultants] = useState(false);
 
-    const API_BASE_URL = 'https://seitek-aka-tyaz.vercel.app'
+    const API_BASE_URL = 'https://seitek-aka-tyaz.vercel.app/api'
 
     // Fetch data from server
     const fetchAreas = async () => {
